01d239d4fc110f90d0d4ae3a3af2ad5355b51cc0
rcsfc <horizon@Ron_Laptop.localdomain>
2022-04-24 17:20:56 -0400

    Converted to a module and updated configs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+# The bucket name is generated using bucket_prefix so there will be a random string appended to it to prevent users from creating the same bucket. This will output the name of that.
+output "bucket" {
+   value = module.vpc-ec2-s3.bucket
+}
+
+# Output the SSH command for the user so they can copy and paste to get to their instance
 output "ssh_command" {
-  value = ["Run this command to connect: ssh -i ~/.ssh/demo_ssh_key.pub.pem ubuntu@${aws_instance.demo_instance.public_ip}"]
+   value = module.vpc-ec2-s3.ssh_command
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,23 @@
+# Sets our availability zone
 variable "availability_zone" {
-     type    = string
-     default = "us-east-1"
+  type = string
+  default = "us-east-1"
 }
 
+# Sets our default instance type. Increase this as you see fit
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+
+# Path to the SSH key we generate during the deployment
 variable "demo_key_path" {
-  default = "~/.ssh/demo_ssh_key.pub.pem"
+  default = "~/.ssh/demo_ssh_key.pub.pem" 
   sensitive = true
+}
+
+# The general naming convention of our bucket will be "push-button-demo-somerandomstring". Change this to suit your needs
+variable "bucket_prefix" {
+  type = string
+  default = "push-button-demo-"
 }
