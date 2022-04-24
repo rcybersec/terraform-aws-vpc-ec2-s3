d514018fec1df5fe7643cf8e10a78dfaeec9fc35
rcsfc <horizon@Ron_Laptop.localdomain>
2022-04-24 18:27:02 -0400

    Changed to github url

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "vpc-ec2-s3" {
-  source = "rcsfc/vpc-ec2-s3/aws"
+  source = "github.com/rcsfc/terraform-aws-vpc-ec2-s3//modules/vpc-ec2-s3"
   instance_type = var.instance_type
   availability_zone = var.availability_zone
   demo_key_path = var.demo_key_path
